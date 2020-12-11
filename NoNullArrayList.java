9150568b836bef3903c7af6b53cdf801cf081166
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-10 19:33:24 -0500

    added second add

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -5,16 +5,20 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public void add(int index, T element) {
     value = element;
     if (value == null) {
-      throw new IllegalArgumentException("Your element is null");
+      throw new IllegalArgumentException("Your element is null :(");
     }
     super.add(index, value);
   }
-}
 
-  // public boolean add(T t) {
-  //
-  // }
+  public boolean add(T t) {
+    value = t;
+    if (value == null) {
+      throw new IllegalArgumentException("Your element is null :(");
+    }
+    return true;
+  }
   //
   // public T set (int index, T element) {
   //
   // }
+}
