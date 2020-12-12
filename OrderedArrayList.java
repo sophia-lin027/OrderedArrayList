ef53bde269133eb14ecdb368ed34828e1109f527
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-11 22:10:03 -0500

    added helper

---
+++ OrderedArrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  private int helper(T value){
+    int index = 0;
+    for(int i = 0; i < this.size(); i ++){
+      if(value.compareTo(this.get(i)) < 0){
+        index = i;
+        return index;
+      }
+    }
+    return this.size();
+  }
+}
