93b0de1097fd98ea51e51d1542fe4c7064797e9d
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-14 00:10:23 -0500

    slight changes, added throw

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,36 +3,40 @@ import java.util.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   private int helper(T value){
     int index = 0;
-    for(int i = 0; i < this.size(); i ++){
-      if(value.compareTo(this.get(i)) < 0){
+    for(int i = 0; i < super.size(); i++){
+      if(value.compareTo(super.get(i)) < 0){
         index = i;
         return index;
       }
     }
-    return this.size();
+    return super.size();
   }
 
   public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Your element is null :(");
+    }
     int ind = helper(element);
     super.add(ind, element);
   }
 
   public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Your element is null :(");
+    }
     int ind = helper(element);
-    super.add(ind,element);
+    super.add(ind, element);
     return true;
   }
 
   public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Your element is null :(");
+    }
     int ind = helper(element);
-    T temp = this.get(index);
-    if (ind == 0){
-      super.add(element);
-    }
-    else{
-      super.add(ind,element);
-    }
-    this.remove(index);
+    T temp = super.get(index);
+    super.add(ind, element);
+    super.remove(temp);
     return temp;
   }
 
